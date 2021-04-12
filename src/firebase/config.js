e4917019cebd2fdbcb7bc1553fc79eb9c0450485
Pooja N G <PoojaNandihalliGirish99@users.noreply.github.com>
2021-04-12 20:58:37 +0530

    firebase config code

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+  import * as firebase from 'firebase/app';
+  import 'firebase/storage';
+  import 'firebase/firestore';
+  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBYzTB8RqlPwloHUkuozyZ5vDzLHMhkOfY",
+    authDomain: "rf-gallery.firebaseapp.com",
+    projectId: "rf-gallery",
+    storageBucket: "rf-gallery.appspot.com",
+    messagingSenderId: "288703060460",
+    appId: "1:288703060460:web:4ba064a1f44a86418808c9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projStorage = firebase.storage();
+  const projFirestore = firebase.firestore();
+
+  export {projStorage, projFirestore};
