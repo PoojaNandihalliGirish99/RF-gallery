57d902708dac358901782f349699d812ef7eb3fe
Pooja N G <PoojaNandihalliGirish99@users.noreply.github.com>
2021-04-12 22:57:37 +0530

    hooks for file handling, error and url  handling

---
+++ src/comps/uploadForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const UploadForm = () => {
+
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    //array of allowed file types
+    const types = ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+        //set state only if we have file inside selected. If there, is it a allowed file type.
+        if(selected && types.includes(selected.type)){ 
+            setFile(selected);
+            setError('');
+        } else{
+            setFile(null);
+            setError("Please select a png/jpeg file format");
+        }
+      }
+
+return (
+    <form>
+    <label>
+    <input type="file" onChange={changeHandler}/>
+    <span>+</span>
+    </label>
+    <div className="output">
+    {error && <div className="error">{error}</div>} 
+    {file && <div>{file.name}</div>} 
+
+    </div>
+    </form>
+)
+
+};
+
+export default UploadForm;
---
+++ src/hooks/useStorage.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import { projStorage } from '../firebase/config'
+
+//hooks to handle file uploads
+
+const useStorage = (file) => {
+    const [progress, setProgress] = useState(0);
+    const [error, setError] = useState(null);
+    const [url, setUrl] = useState(null);
+    
+    // useEffect is a hook that takes two parameters: function and dependency
+    //everytime the dependency changes - the function gets fired
+    //here everythime we have change file value
+    useEffect(()=>{
+        //getting reference to where file should be saved
+        const storageRef = projStorage.ref(file.name);
+
+        //Asynchronous - needs some time to process, so on() is a listener when there is a change in progress,
+        //1st arg. of listener is the type of event,
+        //2nd arg. is a function that is triggered when there is change in event
+        //3rd arg. is the function that is triggered when there is an error
+        //4th arg. is the function that gets triggered 
+        //snapshot object - time of upload of file
+        storageRef.put(file).on('state_changed', (snap)=>{
+            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+            setProgress(percentage);
+        },(error)=>{
+            setError(error);
+        },async()=>{
+            const url = await storageRef.getDownloadURL();
+            setUrl(url);
+
+        })
+
+    },[file]);
+
+    return {progress, url, error}
+
+
+}
+
+export default useStorage;
