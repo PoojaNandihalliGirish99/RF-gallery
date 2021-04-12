454d7ee5e722c2aec17c06b58bb4ab3e5dce15b6
Pooja N G <PoojaNandihalliGirish99@users.noreply.github.com>
2021-04-12 23:20:55 +0530

    new component added
    progress-bar

--- src/comps/uploadForm.js
+++ src/comps/uploadForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ProgressBar from './ProgressBar';
 
 const UploadForm = () => {
 
@@ -17,7 +18,7 @@ const UploadForm = () => {
         } else{
             setFile(null);
             setError("Please select a png/jpeg file format");
-        }
+        } 
       }
 
 return (
@@ -27,9 +28,9 @@ return (
     <span>+</span>
     </label>
     <div className="output">
-    {error && <div className="error">{error}</div>} 
+{error && <div className="error">{error}</div>} 
     {file && <div>{file.name}</div>} 
-
+    {file && <ProgressBar file="file" setFile={setFile}/>}
     </div>
     </form>
 )
